c0eded5c70253d01a520e87cf09ccbc030511377
Vivi Ramos <ramosluzviviane@gmail.com>
2022-01-11 10:38:59 -0300

    add second parameter in test, doc and fix func

--- Iteracao/iteration.go
+++ Iteracao/iteration.go
@@ -1,8 +1,10 @@
 package iteracao
 
-func Repeat(txt string) string {
+// Receive two parameters, first the string that will be repeat.
+// Second a int wich represents the number of times that the string be repeat
+func Repeat(txt string, rep int) string {
 	var res string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < rep; i++ {
 		res += txt
 	}
 	return res
--- Iteracao/iteration_test.go
+++ Iteracao/iteration_test.go
@@ -1,14 +1,29 @@
 package iteracao
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestIteracao(t *testing.T) {
 	t.Run("Shoul repeat a caracter 5 times", func(t *testing.T) {
-		got := Repeat("a")
-		expected := "aaaaa"
+		got := Repeat("a", 6)
+		expected := "aaaaaa"
 
 		if got != expected {
 			t.Errorf("Expected %s, got %s", expected, got)
 		}
 	})
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("b", 5)
+	}
+}
+
+func ExampleRepeat() {
+	res := Repeat("vi", 2)
+	fmt.Println(res)
+	// Output vivi
 }
