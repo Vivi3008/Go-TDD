987d4d2789695b6d18a49060f2e686d41f13836d
Vivi Ramos <ramosluzviviane@gmail.com>
2022-01-12 18:21:54 -0300

    fix: refactor test

--- Arrays_e_Slices/slice_test.go
+++ Arrays_e_Slices/slice_test.go
@@ -6,29 +6,32 @@ import (
 )
 
 func TestArrayESlice(t *testing.T) {
+	verificaResultado := func(t *testing.T, got, expected []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, expected) {
+			t.Errorf("Expected %v, got %v", expected, got)
+		}
+	}
+
 	t.Run("Array de cinco numeros", func(t *testing.T) {
 		numbers := []int{1, 2, 3, 4, 5}
 		got := Soma(numbers)
 		expected := 15
 
 		if got != expected {
-			t.Errorf("Expected %d, got %d, data %v", expected, got, numbers)
+			t.Errorf("Expected %d, got %d", expected, got)
 		}
 	})
 	t.Run("Funcao soma tudo", func(t *testing.T) {
 		got := SomaTodoResto([]int{1, 2}, []int{0, 9})
 		expected := []int{2, 9}
 
-		if !reflect.DeepEqual(got, expected) {
-			t.Errorf("Expected %v, got %v", expected, got)
-		}
+		verificaResultado(t, got, expected)
 	})
 	t.Run("Soma slices vazios", func(t *testing.T) {
 		got := SomaTodoResto([]int{}, []int{1, 3, 5})
 		expected := []int{0, 8}
 
-		if !reflect.DeepEqual(got, expected) {
-			t.Errorf("Expected %v, got %v", expected, got)
-		}
+		verificaResultado(t, got, expected)
 	})
 }
