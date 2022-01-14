b20845ad5488b31fd4b79401466cb81503d2016d
Vivi Ramos <ramosluzviviane@gmail.com>
2022-01-14 17:10:52 -0300

    feat: refactor to auxiliar func

--- Ponteiros/ponteiros_test.go
+++ Ponteiros/ponteiros_test.go
@@ -5,14 +5,36 @@ import (
 )
 
 func TestPonteiros(t *testing.T) {
-	t.Run("Teste Carteira", func(t *testing.T) {
-		c := Carteira{}
-		c.Depositar(50)
-		got := c.Saldo().String()
-		expected := "50 BTC"
-
+	verificaSaldo := func(t *testing.T, c Carteira, expected string) {
+		t.Helper()
+		got := c.GetSaldo().String()
 		if got != expected {
 			t.Errorf("Expected %s, got %s", expected, got)
 		}
+	}
+
+	confirmaErro := func(t *testing.T, err error) {
+		if err == nil {
+			t.Errorf("Expected %s, got nil", err)
+		}
+	}
+	t.Run("Teste Carteira Depositar", func(t *testing.T) {
+		c := Carteira{}
+		c.Depositar(50)
+		expected := "50 BTC"
+		verificaSaldo(t, c, expected)
+	})
+	t.Run("Teste carteira Retirar", func(t *testing.T) {
+		cart := Carteira{saldo: 20}
+		cart.Retirar(10)
+		expected := "10 BTC"
+		verificaSaldo(t, cart, expected)
+	})
+	t.Run("Retirar valor com saldo insuficiente", func(t *testing.T) {
+		cart := Carteira{saldo: 20}
+		err := cart.Retirar(100)
+		verificaSaldo(t, cart, "20 BTC")
+		confirmaErro(t, err)
+
 	})
 }
