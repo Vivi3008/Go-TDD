13bd54189b6bdfcc3719651fba487bba2273c41f
Vivi Ramos <ramosluzviviane@gmail.com>
2022-01-17 11:41:53 -0300

    fix: refactor to helper function

--- Mapas/mapas_test.go
+++ Mapas/mapas_test.go
@@ -5,11 +5,17 @@ import "testing"
 func TestMapas(t *testing.T) {
 	t.Run("Teste Mapas", func(t *testing.T) {
 		dicionario := map[string]string{"Teste": "Isso é apenas um teste"}
+
 		got := Busca(dicionario, "Teste")
 		expected := "Isso é apenas um teste"
 
-		if got != expected {
-			t.Errorf("Expected %s, got %s", expected, got)
-		}
+		comparaStrings(t, expected, got)
 	})
+}
+
+func comparaStrings(t *testing.T, expected, got string) {
+	t.Helper()
+	if got != expected {
+		t.Errorf("Expected %s, got %s", expected, got)
+	}
 }
