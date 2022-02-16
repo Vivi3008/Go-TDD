1e0c64677eb75e00ae6992172fe06fec7ff12a25
Vivi Ramos <ramosluzviviane@gmail.com>
2022-02-16 18:15:24 -0300

    feat: cancel store with internal context

--- Contexto/contexto.go
+++ Contexto/contexto.go
@@ -12,9 +12,21 @@ type Store interface {
 
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		store.Cancel()
-		fmt.Println(store.Fetch())
+		ctx := r.Context()
+
+		data := make(chan string, 1)
+
+		go func() {
+			data <- store.Fetch()
+		}()
+
+		select {
+		case d := <-data:
+			fmt.Fprint(w, d)
+		case <-ctx.Done():
+			store.Cancel()
+		}
 	}
 }
 
-//https://larien.gitbook.io/aprenda-go-com-testes/primeiros-passos-com-go/contexto#escreva-codigo-o-suficiente-para-fazer-o-teste-passar
+//https://larien.gitbook.io/aprenda-go-com-testes/primeiros-passos-com-go/contexto#escreva-o-teste-primeiro-1
--- Contexto/contexto_test.go
+++ Contexto/contexto_test.go
@@ -11,6 +11,7 @@ import (
 type SpyStore struct {
 	response  string
 	cancelled bool
+	t         *testing.T
 }
 
 func (s *SpyStore) Fetch() string {
@@ -22,10 +23,25 @@ func (s *SpyStore) Cancel() {
 	s.cancelled = true
 }
 
+func (s *SpyStore) assertWasCancelled() {
+	s.t.Helper()
+	if !s.cancelled {
+		s.t.Errorf("store foi avisada para cancelar")
+	}
+}
+
+func (s *SpyStore) assertWasNotCancelled() {
+	s.t.Helper()
+	if s.cancelled {
+		s.t.Errorf("store não foi avisada para cancelar")
+	}
+}
+
 func TestContexto(t *testing.T) {
 	var data = "Hello world"
 	t.Run("Contexto", func(t *testing.T) {
-		svr := Server(&SpyStore{response: data})
+		store := &SpyStore{response: data, t: t}
+		svr := Server(store)
 
 		request := httptest.NewRequest(http.MethodGet, "/", nil)
 		response := httptest.NewRecorder()
@@ -35,23 +51,22 @@ func TestContexto(t *testing.T) {
 		if response.Body.String() != data {
 			t.Errorf("Expected %s, got %s", data, response.Body.String())
 		}
+
+		store.assertWasNotCancelled()
 	})
 	t.Run("avisa store para cancelar o trabalho se a request for cancelada", func(t *testing.T) {
-		store := &SpyStore{response: data}
+		store := &SpyStore{response: data, t: t}
 		svr := Server(store)
 
 		request := httptest.NewRequest(http.MethodGet, "/", nil)
 
 		cancellingCtx, cancel := context.WithCancel(request.Context())
 		time.AfterFunc(5*time.Millisecond, cancel)
-		rqcopy := request.WithContext(cancellingCtx)
+		request = request.WithContext(cancellingCtx)
 
 		response := httptest.NewRecorder()
-		svr.ServeHTTP(response, rqcopy)
+		svr.ServeHTTP(response, request)
 
-		if !store.cancelled {
-			t.Errorf("Store nao foi avisada para cancelar")
-		}
-
+		store.assertWasCancelled()
 	})
 }
